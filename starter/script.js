72f3b94c93cef04a5243a25c90cf77e076029e3b
Damenhua <154488852+Damenhua@users.noreply.github.com>
2024-08-03 21:23:22 +0800

    displayMovement

--- starter/script.js
+++ starter/script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovement = function (movement) {
+  containerMovements.innerHTML = '';
+
+  movement.forEach(function (n, i) {
+    const type = n > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+       <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${n} €</div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovement(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
